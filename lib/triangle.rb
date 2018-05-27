174115cc3e89a6b351408f2cadfc36fe189ff084
taryn yaeger <tsyaeger@gmail.com>
2018-05-27 05:12:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+
+
 class Triangle
-  # write code here
+  
+  attr_accessor :sides
+  
+  def initialize(a,b,c)
+    @sides = []
+    @sides << a.to_f
+    @sides << b.to_f
+    @sides << c.to_f
+  end
+  
+  
+  def kind 
+    
+    perms = @sides.permutation.to_a 
+    puts perms.inspect
+    
+    if @sides.any?{|side| side < 0} || perms.any?{|perm| perm[0] + perm[1] < perm[2]}
+      puts "nope"
+       
+    else
+      
+      if sides.uniq.length == 1
+        kind = "equilateral"
+      elsif sides.uniq.length == 2
+      	kind = "isosceles"
+      else
+        kind = "scalene"
+      end
+      puts kind
+    end 
+    
+  end
+  
+end
+
+class TriangleError < StandardError
 end
